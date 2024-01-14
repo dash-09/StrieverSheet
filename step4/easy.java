98f0ab5f83ac76f1ec9711e376bd2233167fbcbb
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2024-01-14 11:23:50 +0530

    Update easy.java

--- step4/easy.java
+++ step4/easy.java
@@ -69,5 +69,114 @@ public static void main (String[] args) {
 
 }
 
-//  4) search insert position 
+// 4) search insert position
+ class Solution {
+    public int searchInsert(int[] nums, int target) {
+         int low = 0;
+         int mid = 0;
+         int high = nums.length -1 ;
+         while(low<= high){
+             mid = (low+high)/2 ;
+             if(target < nums[mid]){
+             high = mid-1 ;
+             }else if(target == nums[mid]){
+             return mid;
+             }else{
+                 low = mid+1;
+             }
+         }
+         return low ;
+    }
+}
+// 5) floor/ ceil in sorted array 
+// 6) find first and last occurance of a number in sorted array
+// 7) count occurances of a number in a sorted array with duplicates
+// 8) search in rotated sorted array (i)(no duplicates)
+class Solution {
+    public int search(int[] nums, int target) {
+         int start = 0 ; int end = nums.length -1 ;
+        while(start<=end){
+            int mid =(start+end)/2 ;
+            if(nums[mid] == target){
+                return mid ;
+            }else if(nums[mid]>= nums[start]){
+                if(nums[start] <= target && target < nums[mid]){
+                    end = mid-1 ;
+                }else{
+                    start = mid+1 ;
+                }
 
+            }else{
+                if(nums[mid]< target && target <= nums[end]){
+                    start = mid+1 ;
+                }else{
+                    end = mid-1 ;
+                }
+            }
+        }
+        return -1 ;
+    }
+
+}
+// 9) search in rotated sorted array (ii)
+// 10) find minimum in rotated sorted array 
+// 11) find out how many times array has been rotated
+  class solution{
+    public static void main(String args[]){
+       int arr[] = {4,5,6,7,0,1,2,3}; int n = arr.length;
+        if(arr.length == 0) System.out.print(0);
+        if(arr.length == 1) System.out.print(0);
+         int low = 1; int high = n-2;
+         int ans = -10;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]<arr[mid-1]&& arr[mid]<arr[mid+1]){
+           ans = mid;break;
+            }else if(arr[mid]>arr[mid-1]){low = mid+1;}
+            else {high = mid-1;}
+        }
+         
+           System.out.print(ans);
+        
+    }
+  }        
+
+// 12) single element in sorted array 
+  class solution{
+    public int singleElement(int arr[]){
+       if(arr.length == 0)return 0;
+       if(arr.length == 1)return arr[0];
+       if(arr.length == 2)return Math.max(arr[0],arr[1]);
+       if(arr[0]!=arr[1])return arr[0];
+       if(arr[arr.length-1]!=arr[arr.length-2])return arr[arr.length-1]; 
+        int low = 1; int high = arr.length-2;
+        while(low<=high){
+           int mid = (low+high)/2;
+            if(arr[mid]!= arr[mid-1] && arr[mid] != arr[mid+1]){return arr[mid];}
+            else if((arr[mid] != arr[mid-1] && mid%2== 0)|| (arr[mid] != arr[mid+1] && mid%2== 1)){low = mid+1;}
+            else{high = mid-1;}
+        }
+        return -1;
+    }
+  }  
+// 13) find peak element
+class solution{
+    public int peakElement(int arr[]){
+       if(arr.length == 0)return 0;
+       if(arr.length == 1)return arr[0];
+       if(arr.length == 2)return Math.max(arr[0],arr[1]);
+       if(arr[0]>arr[1])return arr[0];
+       if(arr[arr.length-1]>arr[arr.length-2])return arr[arr.length-1];
+      int low = 1; int high = arr.length-2;
+       while(low<=high){
+          int mid = (low+high)/2;
+           if(arr[mid]>arr[mid-1] && arr[mid+1] < arr[mid]){
+                return arr[mid];
+           }else if(arr[mid]>arr[mid-1]){low = mid+1;}
+           else{high = mid-1;}               
+       }
+        return -1;
+
+    }}
+
+ 
