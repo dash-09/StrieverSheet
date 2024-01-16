f315d0997ff07f5f28191cc27885a4205f045655
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2024-01-16 11:10:28 +0530

    Update easy.java

--- step5/easy.java
+++ step5/easy.java
@@ -1,1 +1,8 @@
 
+// Remove outermost Paranthesis	
+// Reverse Words in a String
+// Largest odd number in a string	
+//  Longest Common Prefix	
+// Isomorphic String	
+// check whether one string is a rotated or not
+// Check if two Strings are anagrams of each other
