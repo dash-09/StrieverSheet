6936a03a6f6362cb0a427cc321c69f6f6e98df15
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2024-01-22 13:48:15 +0530

    Update medium.java

--- step5/medium.java
+++ step5/medium.java
@@ -1,5 +1,23 @@
 // Sort Characters by frequency	
+
 // Maximum Nesting Depth of Paranthesis
+class Solution{
+  public static int depth(String s){
+     int count = 0, max = Integer.MIN_VALUE;
+     Stack<Character> st = new Stack<>();
+    for(int i = 0;i<s.length();i++){
+      if(s.charAt(i)=='('){
+       st.push('(');
+        count++;
+        max = Math.max(max,count);
+       }else if(s.charAt(i)==')'){
+         st.pop();
+        count--;
+      }      
+    }
+    return max;
+  }
+}
 // Roman Number to Integer and vice versa
 // Implement Atoi	
 // Count Number of Substrings	
