6482f4681aeb9d484036fa7cc61002f29cf08345
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2024-01-22 14:32:09 +0530

    Update medium.java

--- step5/medium.java
+++ step5/medium.java
@@ -1,5 +1,64 @@
 // Sort Characters by frequency	
 
+class Solution {
+    public String frequencySort(String s) {
+
+        /*
+
+          Map<Character, Integer> map = new HashMap<>();
+          for (int i = 0; i < s.length(); i++) {
+          map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+          }
+          
+          PriorityQueue<Map.Entry<Character,Integer>> pq = new PriorityQueue<>((a,b)->
+          b.getValue()-a.getValue());
+          pq.addAll(map.entrySet());
+          StringBuilder sb = new StringBuilder();
+          while(!pq.isEmpty()){
+          Map.Entry e = pq.poll();
+          for(int i = 0;i<(int)e.getValue();i++){
+          sb.append(e.getKey());
+          }
+          
+          }
+          return sb.toString();
+         */
+
+        Map<Character, Integer> map = new HashMap<>();
+         for (int i = 0; i < s.length(); i++) {
+          map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+          }
+
+        List<Character>[] list = new ArrayList[s.length()+1];
+          for(Character ch : map.keySet()){
+              int charr= map.get(ch);
+              if(list[charr]==null){
+                  list[charr] = new ArrayList<>();
+              }
+              list[charr].add(ch);
+          }
+
+          StringBuilder sb = new StringBuilder();
+
+          for(int i = list.length-1;i>=0;i--){
+              if(list[i]!=null){
+                  for(char ch: list[i]){
+                      for(int j = 0;j<map.get(ch);j++){
+                          sb.append(ch);
+                      }
+                  }
+              }
+              
+          }
+              
+return sb.toString();
+
+
+
+    }
+}
+
+
 // Maximum Nesting Depth of Paranthesis
 class Solution{
   public static int depth(String s){
