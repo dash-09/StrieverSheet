109dc8c7aa9cd07e21438b11f5913737d9101758
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2024-01-22 14:41:56 +0530

    Update medium.java

--- step5/medium.java
+++ step5/medium.java
@@ -4,7 +4,6 @@ class Solution {
     public String frequencySort(String s) {
 
         /*
-
           Map<Character, Integer> map = new HashMap<>();
           for (int i = 0; i < s.length(); i++) {
           map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
@@ -52,12 +51,8 @@ class Solution {
           }
               
 return sb.toString();
-
-
-
     }
 }
-
 
 // Maximum Nesting Depth of Paranthesis
 class Solution{
@@ -78,8 +73,36 @@ class Solution{
   }
 }
 // Roman Number to Integer and vice versa
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int ans = 0;
+
+         for(int i = 0;i<s.length();i++){
+             if(i<s.length()-1 && map.get(s.charAt(i))<map.get(s.charAt(i+1))){
+                 ans -= map.get(s.charAt(i));
+             }else{
+                 ans += map.get(s.charAt(i));
+             }
+         }
+   return ans;
+    }
+}
 // Implement Atoi	
+
+
+
 // Count Number of Substrings	
 // Longest Palindromic Substring[Do it without DP]
 // Sum of Beauty of all substring	
+
+
+
 // Reverse Every Word in A String	 (already done in easy part of this section)
