fb728c041a8c4936e475722cbcb2321b64318d69
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2024-01-24 18:07:54 +0530

    Update medium.java

--- step4/medium.java
+++ step4/medium.java
@@ -1,1 +1,14 @@
-
+// finding square root of a number using binary search;
+// find nth root of a number using binary search
+// koko eating bananas
+// minimum days to make M banquates
+// find a smallest divisor
+// capacity to ship packages within D days
+// kth missing positive number 
+// aggresive cows 
+// book allocation problem
+// split array
+// painter's problem
+// minimise maximum distance to gas station
+// median of 2 sorted arrays
+// kth element of two sorted arrays  
