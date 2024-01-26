95275f439c5b7488bb2258cef991186609233bf5
Adarsh Shukla <74849401+dash-09@users.noreply.github.com>
2024-01-26 19:08:36 +0530

    Update medium.java

--- step4/medium.java
+++ step4/medium.java
@@ -1,6 +1,55 @@
-// finding square root of a number using binary search;
+// finding square root of a number using binary search
+class Solution{
+  public static void main(String args[]){
+    int n = 36 ;  
+    // output = 6 
+    int start = 1, end = n-1;
+    while(start<=end){
+       long mid = (start+end)/2;
+       long temp = mid*mid;
+      if(temp<=(long)n{
+        start = (int)mid+1;     
+      }else{ end = (int)mid-1;}
+    } 
+    System.out.print(high);
+  }
+}
 // find nth root of a number using binary search
+
 // koko eating bananas
+
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int lo = 1;
+        int hi = Integer.MIN_VALUE;
+        for(int pile:piles){
+            hi = Math.max(hi, pile);
+        } 
+        while (lo < hi) {
+            int K = lo + ((hi - lo) >> 1);
+            if (canEatAll(piles, K, h)) {
+                hi = K;
+
+            } else {
+                lo = K + 1;
+            }
+        }
+
+        return lo;
+    }
+
+    private boolean canEatAll(int[] piles, int K, int H) {
+        int countHour = 0;
+        for (int pile : piles) {
+            countHour += pile / K;
+            if (pile % K != 0)
+                countHour++;
+        }
+        return countHour <= H;
+    }
+
+}
+
 // minimum days to make M banquates
 // find a smallest divisor
 // capacity to ship packages within D days
